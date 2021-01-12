efdf340acda4dd544d9c9043be9c087a78f79aba
brunommbruno <brunommbruno@gmail.com>
2021-01-12 15:45:09 +0000

    created project model table

---
+++ database/migrations/2021_01_12_153920_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("title", 20);
+            $table->string("description");
+            $table->string("web_link");
+            $table->string("code_link");
+            $table->string("project_img");
+            $table->string("tags");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
