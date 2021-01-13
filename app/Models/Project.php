5a1a2f94a8292b6edf2f977f636a4b4540dcf8f3
brunommbruno <brunommbruno@gmail.com>
2021-01-13 16:25:00 +0000

    added fillable property to project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["title", "description", "web_link", "code_link", "project_img", "tags"];
 }
